e9ef80d5e633d5407b4f8d0969998ca1f47ffcf0
zhihaofans <zhihaofans@hotmail.com>
2022-10-30 12:38:23 +0800

    语言设为简体中文

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { NConfigProvider, zhCN, dateZhCN } from "naive-ui";
 import { RouterView } from "vue-router";
 import TopBar from "./components/TopBar.vue";
 </script>
-
 <template>
-  <header>
-    <TopBar />
-  </header>
-  <body>
-    <RouterView />
-  </body>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <header>
+      <TopBar />
+    </header>
+    <body>
+      <RouterView />
+    </body>
+  </n-config-provider>
 </template>
